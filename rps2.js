5091de96b75a9a0984f6954f7310b34d89bed74f
alcarson1023 <alcarson1023@yahoo.com>
2020-11-24 09:02:49 -0600

    Added a UI

--- rps2.js
+++ rps2.js
@@ -1,53 +1,81 @@
-function fullGame(){
-    let gamesPlayed = 0;
+// Starting variables & functions
     let userScore = 0;
     let computerScore = 0;
+    let gamesCounter = 0
+    const buttons = document.querySelectorAll('.selection');
+    const games = document.querySelector('#games');
+    const winner = document.querySelector('#winner');
+    const results = document.querySelector('#results');
+    const reset = document.querySelector('#reset');
+    const btnBox = document.querySelector('#btnBox');
 
-    while(gamesPlayed < 5){
+// Whenever a button is pushed, set that button as the user's choice.
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            userChoice = button.id;
+            oneGame(userChoice)
+        });
+    });
+
+// When the reset button is pushed, set everything back to zero.
+    reset.addEventListener('click', function() {
+        userScore = 0;
+        computerScore = 0;
+        gamesCounter = 0;
+        winner.textContent = ``;
+        results.textContent = ``;
+        games.textContent = gamesCounter
+        reset.style.cssText = 'display: none;'
+        btnBox.style.cssText = 'display: block;'
+    });
+
+    function oneGame(userChoice){
         let computerChoice = ''
+        
         // Uses the current time in milliseconds to generate a random number based on the last 2 digits.
         const randomNum = Date.now() % 100
         randomNum < 33 ? computerChoice = 'rock' :
         randomNum < 66 ? computerChoice = 'paper' :
         computerChoice = 'scissors'
 
-        // Accept the user's choice, convert it to lowercase & trim it. Don't proceed until it's valid.
-        let userChoice = prompt('Please enter "Rock", "Paper", or "scissors".', '').toLowerCase().trim()
-        while(userChoice != 'rock' && userChoice != 'paper' && userChoice != 'scissors'){
-            userChoice = prompt('Your input was not valid.\nPlease enter "Rock", "Paper", or "scissors".', '').toLowerCase().trim()
-            return(gamesPlayed, userScore, computerScore)
-        }   
-
         // If statements to find the winner. If it's a tie, return tie.
         if(userChoice == computerChoice){
-            alert(`User chose: ${userChoice}. Computer chose: ${computerChoice}. It's a tie!`)
+            results.textContent = `User chose: ${userChoice}. Computer chose: ${computerChoice}. It's a tie!`
         }
         // If it's not a tie, check to see if the user won. Otherwise, computer wins.
         else if(userChoice == 'rock' && computerChoice == 'scissors'){
-            alert(`User chose: ${userChoice}. Computer chose: ${computerChoice}. User wins!`);
+            results.textContent = `User chose: ${userChoice}. Computer chose: ${computerChoice}. User wins!`;
             userScore ++;
         }
         else if(userChoice == 'paper' && computerChoice == 'rock'){
-            alert(`User chose: ${userChoice}. Computer chose: ${computerChoice}. User wins!`);
+            results.textContent = `User chose: ${userChoice}. Computer chose: ${computerChoice}. User wins!`;
             userScore ++;
         }
         else if(userChoice == 'scissors' && computerChoice == 'paper'){
-            alert(`User chose: ${userChoice}. Computer chose: ${computerChoice}. User wins!`);
+            results.textContent = `User chose: ${userChoice}. Computer chose: ${computerChoice}. User wins!`;
             userScore ++;
         }
         else{
-            alert(`User chose: ${userChoice}. Computer chose: ${computerChoice}. Computer wins!`);
+            results.textContent = `User chose: ${userChoice}. Computer chose: ${computerChoice}. Computer wins!`;
+            computerScore ++;
         }
-        
-        gamesPlayed ++
+        gamesCounter += 1
+        games.textContent = gamesCounter
+
+        // Perform end-of-game actions (score reporting, game reset)
+        if(gamesCounter === 5){
+            // Check the scores, report the winner.
+            if(computerScore > userScore){
+                winner.textContent = `Computer wins ${computerScore} to ${userScore}`
+            }
+            else if(computerScore < userScore){
+                winner.textContent = `User wins ${userScore} to ${computerScore}`
+            }
+            else{
+                winner.textContent = `It's a tie! ${userScore} to ${computerScore}`
+            }
+            reset.style.cssText = 'display: block;'
+            btnBox.style.cssText = 'display: none;'
+        }
     }
-    // Check the scores, report the winner.
-    if(computerScore > userScore){
-        alert(`Computer wins ${computerScore} to ${userScore}`)}
-    else if(computerScore < userScore){
-        alert(`User wins ${userScore} to ${computerScore}`)}
-    else{
-        alert(`It's a tie! ${userScore} to ${computerScore}`)
-    }
-}
-fullGame()
